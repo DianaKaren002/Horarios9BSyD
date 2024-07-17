aa4ed99cf2ed47065ff374ec2fe311485a91ab9a
DAVOPENUDO <utp0144150@alumno.utpuebla.edu.mx>
2024-07-17 07:48:12 -0600

    conexion a base de datos de mysql

---
+++ ClassBLLHorariosMySQL/BLLHorarios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassDALMySql;
+
+namespace ClassBLLHorariosMySQL
+{
+    public class BLLHorarios
+    {
+        private string cad = "";
+        private DALMySQL objdal = null;
+        public BLLHorarios(string cadeconex)
+        {
+            cad = cadeconex;
+            objdal = new DALMySQL(cad);
+        }
+        public string probarConexion()
+        {
+            string mensaje = "";
+            objdal.abrirConexion(ref mensaje);
+            return mensaje;
+        }
+
+    }
+}
